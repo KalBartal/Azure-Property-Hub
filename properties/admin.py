605c58a106fa386f7d1b6d2ca9620bacfd10c244
Kal Bartal <kal.bartal@gmail.com>
2023-03-12 13:50:56 +0000

    Remove ignored files and directories

--- properties/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Property
-
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'created_at')
-
-admin.site.register(Property, PropertyAdmin)
